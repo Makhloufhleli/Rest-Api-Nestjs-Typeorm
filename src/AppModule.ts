024512bcf81c22598fb7443682bc922005198f5a
makhlouf-2022 <mhelali@anypli.com>
2022-10-16 15:59:48 +0100

    02_SWAGGER_UI: Add Swagger UI config and enable swagger in main application file

--- src/AppModule.ts
+++ src/AppModule.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
-
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { mysqlDatabaseConfig } from '@Config/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(mysqlDatabaseConfig),
+  ],
   controllers: [],
   providers: [],
 })
